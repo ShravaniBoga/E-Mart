a4e992d736aff7cca98c4d748eff284fee3a1241
BOGA SHRAVANI <shravaniboga07@gmail.com>
2024-02-22 03:51:27 +0530

    useContext

--- src/store/pages/MobilePage.jsx
+++ src/store/pages/MobilePage.jsx
@@ -3,35 +3,41 @@ import { Link } from "react-router-dom"
 import { mobileData } from '../data/mobiles'
 import Navbar from '../components/Navbar'
 const MobilePage = () => {
-    const [selectedProduct, setSelectedProduct] = useState([]);
+    const [selectedProduct, setSelectedProduct] = useState([])
     const companyHandler = (mango) => {
         if (selectedProduct.includes(mango)) {
             const updatedProduct = selectedProduct.filter(item => item !== mango)
+            setSelectedProduct(updatedProduct)
+
         } else {
             setSelectedProduct([...selectedProduct, mango])
         }
     }
-    const filteredProduct = selectedProduct.length === 0 ? mobileData : mobileData.filter((orange) => selectedProduct.includes(orange.company))
 
+    const filteredProduct = selectedProduct.length === 0 ? mobileData : mobileData.filter((orange) => selectedProduct.includes(orange.company))
     return (
         <>
             <Navbar />
             <div className="fullpage">
                 <div className="pro-selected">
-                    {mobileData.map((phone) => {
+                    {mobileData.map((cooler) => {
                         return (
                             <div>
                                 <label>
-                                    <input type="checkbox" checked={selectedProduct.includes(phone.company)}
-                                        onChange={() => companyHandler(phone.company)} />
+                                    <input type="checkbox"
+                                        checked={selectedProduct.includes(cooler.company)}
+                                        onChange={() => companyHandler(cooler.company)}
+
+                                    />
+                                    {cooler.company}
                                 </label>
-                                {phone.company}
                             </div>
                         )
 
                     })}
                 </div>
-                <div className='pageSection'>
+
+                <div className="pageSection">
                     {filteredProduct.map((item) => {
                         return (
                             <div>
